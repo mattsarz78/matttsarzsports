7f0d5e279e05166b1b45be3459619901d6e9e965
mattsarz78 <matt_sarz44017@yahoo.com>
2016-02-13 20:25:18 -0500

    Update sitemap, update flex schedule link & remove use of MS Ajax

--- MSS/App_Start/BundleConfig.cs
+++ MSS/App_Start/BundleConfig.cs
@@ -10,10 +10,8 @@ namespace MSS
             bundles.UseCdn = true;
             const string jQueryCdn = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.10.0.min.js";
 			const string modernizerCdn = "https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js";
-            const string ajaxCdn = "http://ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js";
 
             bundles.Add(new ScriptBundle("~/bundles/jquery", jQueryCdn).Include("~/Scripts/jquery-{version}.js"));
-            bundles.Add(new ScriptBundle("~/bundles/ajax", ajaxCdn).Include("~/Scripts/MicrosoftAjax.js"));
 			bundles.Add(new ScriptBundle("~/bundles/weekly").Include("~/Scripts/Schedule.js", "~/Scripts/Weekly.js", "~/Scripts/BackToTop.js"));
 			bundles.Add(new ScriptBundle("~/bundles/weektext").Include("~/Scripts/Text.js", "~/Scripts/WeekText.js", "~/Scripts/BackToTop.js"));
 			bundles.Add(new ScriptBundle("~/bundles/daily").Include("~/Scripts/Schedule.js", "~/Scripts/Daily.js", "~/Scripts/BackToTop.js"));
