2e4a5d331d5592c94fe572b08535fa6b1a3a2e85
mattsarz78 <matt_sarz44017@yahoo.com>
2016-09-23 22:54:20 -0400

    Update to jQuery 3.1.0

--- MSS/Scripts/_references.js
+++ MSS/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.0.0.js" />
+﻿/// <reference path="jquery-3.1.0.js" />
 /// <reference path="jquery-ui-1.11.4.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
