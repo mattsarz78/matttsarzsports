d8425a258f86d72a9dbf309b1bda7d24f61b0222
Matthew Sarzyniak <matt_sarz44017@yahoo.com>
2020-04-29 07:04:24 -0400

    Updates

--- MSS/BC/WeeklyTextSchedule.cs
+++ MSS/BC/WeeklyTextSchedule.cs
@@ -7,55 +7,56 @@ namespace MSS.BC
 {
     public class WeeklyTextSchedule : TextSchedule, IWeeklyTextSchedule
     {
-	    readonly IBools _bools;
-	    readonly ISeasonContents _sc;
-	    readonly IStoredProcHelper _sph;
+        readonly IBools _bools;
+        readonly ISeasonContents _sc;
+        readonly IStoredProcHelper _sph;
 
-	    public WeeklyTextSchedule(IBools bools, ISeasonContents sc, IStoredProcHelper sph, ITimeZoneHelper tzh) 
-			: base(bools, tzh)
+        public WeeklyTextSchedule(IBools bools, ISeasonContents sc, IStoredProcHelper sph, ITimeZoneHelper tzh)
+            : base(bools, tzh)
         {
             _bools = bools;
             _sc = sc;
             _sph = sph;
         }
 
-		public ScheduleModel GetWeeklyTextData(int week, string sportYear, string year, string sport, string timeZone)
+        public ScheduleModel GetWeeklyTextData(int week, string sportYear, string year, string sport, string timeZone)
         {
-			var fullYearDates = _sc.CreateDateModel(year);
-			var isBowlWeek = _bools.CheckIfBowlWeek(week, fullYearDates);
-        	var isFootball = sport.Contains("football");
-        	var hasPostseason = _bools.CheckSportYearAttributesBool(sportYear, "hasPostseason");
+            var fullYearDates = _sc.CreateDateModel(year);
+            var isBowlWeek = _bools.CheckIfBowlWeek(week, fullYearDates);
+            var isFootball = sport.Contains("football");
+            var hasPostseason = _bools.CheckSportYearAttributesBool(sportYear, "hasPostseason");
 
-			var textModel = new ScheduleModel
-								{
-									TelevisedGamesList = CreateGamesList(timeZone, week, year, sport),
-									Week = week.ToString(),
-									SportYear = sportYear,
-									IsBowlWeek = isFootball && isBowlWeek,
-                                    IsBasketballPostseason = !isFootball && hasPostseason && _bools.CheckIfBasketballPostseason(week, fullYearDates),
-									IsNIT = !isFootball && _bools.CheckIfNIT(week, fullYearDates)
-			};
-            
+            var textModel = new ScheduleModel
+            {
+                TelevisedGamesList = CreateGamesList(timeZone, week, year, sport),
+                Week = week.ToString(),
+                SportYear = sportYear,
+                Year = year,
+                IsBowlWeek = isFootball && isBowlWeek,
+                IsBasketballPostseason = !isFootball && hasPostseason && _bools.CheckIfBasketballPostseason(week, fullYearDates),
+                IsNIT = !isFootball && _bools.CheckIfNIT(week, fullYearDates)
+            };
+
             return textModel;
         }
 
-		private List<TelevisedGame> CreateGamesList(string timeZone, int week, string year, string sport)
+        private List<TelevisedGame> CreateGamesList(string timeZone, int week, string year, string sport)
         {
             List<TelevisedGame> gameList;
             var parmList = new StoredProcParmList
             {
-                StoredProcParms = new List<StoredProcParm> { 
+                StoredProcParms = new List<StoredProcParm> {
                     new StoredProcParm {ParmName = "@Week", ParmValue = week.ToString()},
                     new StoredProcParm {ParmName = "@Sport", ParmValue = sport},
                     new StoredProcParm {ParmName = "@Season", ParmValue = year},
                 }
             };
-	        using (var conn = new SqlConnection(Constants.ConnString))
-	        using (SqlDataReader resultSet = _sph.RunDataReader(parmList, conn, "GetTVGames"))
-	        {
-				gameList = FillGameList(timeZone, resultSet, false);
-			}
-			return gameList;
+            using (var conn = new SqlConnection(Constants.ConnString))
+            using (SqlDataReader resultSet = _sph.RunDataReader(parmList, conn, "GetTVGames"))
+            {
+                gameList = FillGameList(timeZone, resultSet, false);
+            }
+            return gameList;
         }
     }
 }
