52ffbc5a0c758cb0ecd12f7e6307d760506bc82d
Matthew Sarzyniak <matt_sarz44017@yahoo.com>
2023-06-10 15:38:49 -0400

    Updates for packages

--- MSS/Scripts/_references.js
+++ MSS/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.6.1.js" />
+﻿/// <reference path="jquery-3.7.0.js" />
 /// <reference path="jquery-ui-1.11.4.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
